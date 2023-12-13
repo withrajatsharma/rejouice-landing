9de697e18e98d999ed9a45ffabd0a73adb8f56e4
RajatSharma <withrajatsharma@gmail.com>
2023-12-13 16:05:13 +0530

    loader completed

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
+var tl = gsap.timeline();
+
+
+const time = ()=>{
+    let a =0;
+  setInterval(()=>{
+        if(a<100){
+            a=a+Math.floor(Math.random()*20);
+            document.querySelector("#loader h1").innerHTML=a+"%"
+        }
+        else{
+            a=100;
+            document.querySelector("#loader h1").innerHTML=a+"%";
+            tl.to("#loader",{
+                top:"-100vh",
+                duration:1,
+                delay:.2
+                
+            })
+        }
+    },100)
+}
+
+
+tl.to("#loader h1",{
+    onStart:time(),
+    // duration:1
+})
+
+
+
+
 gsap.to("#page1>h1",{
     transform: "translateX(-90%)",
     fontWeight:"100",
